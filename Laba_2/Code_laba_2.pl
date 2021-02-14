a06cb359d9fe8a5f255e3e971a80dbbed83c1072
KonstantinFisun <72182894+KonstantinFisun@users.noreply.github.com>
2021-02-14 19:39:35 +0300

    Задание 2 Лабораторная 2

---
+++ Laba_2/Code_laba_2.pl
@@ -0,0 +1,743 @@
+fly(spider-man,0).
+fly(iron_man,1).
+fly(hulk,0).
+fly(сaptain_america,0).
+fly(black_widow,0).
+fly(thor,1).
+fly(scarlet_witch,1).
+fly(doctor_strange,1).
+fly(clint,0).
+fly(black_panther,0).
+fly(war_machine,1).
+fly(vision,1).
+fly(nick_fury,0).
+fly(thanos,0).
+fly(groot,0).
+fly(gamora,0).
+fly(loki,0).
+fly(logan,0).
+fly(ultron,1).
+fly(quicksilver,0).
+fly(deadpool,0).
+fly(bucky,0).
+fly(venom,0).
+fly(red_skull,0).
+fly(captain_marvel,1).
+fly(doctor_octopus,0).
+fly(falcon,1).
+fly(rocket_raccoon,0).
+fly(green_goblin,0).
+fly(ant-man,0).
+
+fly(juggernaut,0).
+fly(odin,0).
+fly(mephisto,0).
+fly(nebula,0).
+fly(shuri,0).
+fly(phil_coulson,0).
+fly(wong,0).
+fly(drax,0).
+fly(mantis,0).
+fly(valkyrie,0).
+
+
+
+evil(spider-man, 0).
+evil(iron_man, 0).
+evil(hulk, 2).
+evil(сaptain_america, 0).
+evil(black_widow, 0).
+evil(thor, 0).
+evil(scarlet_witch, 0).
+evil(doctor_strange, 0).
+evil(clint, 0).
+evil(black_panther, 0).
+evil(war_machine, 0).
+evil(vision, 0).
+evil(nick_fury, 0).
+evil(thanos, 1).
+evil(groot, 0).
+evil(gamora, 0).
+evil(loki, 1).
+evil(logan, 0).
+evil(ultron, 1).
+evil(quicksilver, 0).
+evil(deadpool, 0).
+evil(bucky, 0).
+evil(venom, 1).
+evil(red_skull, 1).
+evil(captain_marvel, 0).
+evil(doctor_octopus, 1).
+evil(falcon, 0).
+evil(rocket_raccoon, 0).
+evil(green_goblin, 1).
+evil(ant-man, 0).
+
+evil(juggernaut, 1).
+evil(odin, 0).
+evil(mephisto, 1).
+evil(nebula, 2).
+evil(shuri, 0).
+evil(phil_coulson, 0).
+evil(wong, 0).
+evil(drax, 0).
+evil(mantis, 0).
+evil(valkyrie, 0).
+
+
+
+man(spider-man, 1).
+man(iron_man, 1).
+man(hulk, 2).
+man(сaptain_america, 1).
+man(black_widow, 1).
+man(thor, 2).
+man(scarlet_witch, 1).
+man(doctor_strange, 1).
+man(clint, 1).
+man(black_panther, 1).
+man(war_machine, 1).
+man(vision, 0).
+man(nick_fury, 1).
+man(thanos, 0).
+man(groot, 0).
+man(gamora, 0).
+man(loki, 2).
+man(logan, 1).
+man(ultron, 0).
+man(quicksilver, 1).
+man(deadpool, 1).
+man(bucky, 1).
+man(venom, 0).
+man(red_skull, 1).
+man(captain_marvel, 1).
+man(doctor_octopus, 1).
+man(falcon, 1).
+man(rocket_raccoon, 0).
+man(green_goblin, 1).
+man(ant-man, 1).
+
+man(juggernaut, 1).
+man(odin, 0).
+man(mephisto, 0).
+man(nebula, 0).
+man(shuri, 1).
+man(phil_coulson, 1).
+man(wong, 1).
+man(drax, 0).
+man(mantis, 0).
+man(valkyrie, 2).
+
+
+
+size(spider-man, 0).
+size(iron_man, 0).
+size(hulk, 1).
+size(сaptain_america, 0).
+size(black_widow, 0).
+size(thor, 0).
+size(scarlet_witch, 0).
+size(doctor_strange, 0).
+size(clint, 0).
+size(black_panther, 0).
+size(war_machine, 0).
+size(vision, 0).
+size(nick_fury, 0).
+size(thanos, 0).
+size(groot, 0).
+size(gamora, 0).
+size(loki, 0).
+size(logan, 0).
+size(ultron, 0).
+size(quicksilver, 0).
+size(deadpool, 0).
+size(bucky, 0).
+size(venom, 0).
+size(red_skull, 0).
+size(captain_marvel, 0).
+size(doctor_octopus, 0).
+size(falcon, 0).
+size(rocket_raccoon, 0).
+size(green_goblin, 0).
+size(ant-man, 1).
+
+size(juggernaut, 0).
+size(odin, 0).
+size(mephisto, 0).
+size(nebula, 0).
+size(shuri, 0).
+size(phil_coulson, 0).
+size(wong, 0).
+size(drax, 0).
+size(mantis, 0).
+size(valkyrie, 0).
+
+
+
+earth(spider-man, 1).
+earth(iron_man, 1).
+earth(hulk, 1).
+earth(сaptain_america, 1).
+earth(black_widow, 1).
+earth(thor, 0).
+earth(scarlet_witch, 1).
+earth(doctor_strange, 1).
+earth(clint, 1).
+earth(black_panther, 1).
+earth(war_machine, 1).
+earth(vision, 1).
+earth(nick_fury, 1).
+earth(thanos, 0).
+earth(groot, 0).
+earth(gamora, 0).
+earth(loki, 0).
+earth(logan, 1).
+earth(ultron, 1).
+earth(quicksilver, 1).
+earth(deadpool, 1).
+earth(bucky, 1).
+earth(venom, 0).
+earth(red_skull, 1).
+earth(captain_marvel, 1).
+earth(doctor_octopus, 1).
+earth(falcon, 1).
+earth(rocket_raccoon, 0).
+earth(green_goblin, 1).
+earth(ant-man, 1).
+
+earth(juggernaut, 1).
+earth(odin, 0).
+earth(mephisto, 0).
+earth(nebula, 0).
+earth(shuri, 1).
+earth(phil_coulson, 1).
+earth(wong, 1).
+earth(drax, 1).
+earth(mantis, 0).
+earth(valkyrie, 0).
+
+
+
+avengers(spider-man, 0).
+avengers(iron_man, 1).
+avengers(hulk, 1).
+avengers(сaptain_america, 1).
+avengers(black_widow, 1).
+avengers(thor, 1).
+avengers(scarlet_witch, 0).
+avengers(doctor_strange, 0).
+avengers(clint, 1).
+avengers(black_panther, 0).
+avengers(war_machine, 0).
+avengers(vision, 0).
+avengers(nick_fury, 0).
+avengers(thanos, 0).
+avengers(groot, 0).
+avengers(gamora, 0).
+avengers(loki, 0).
+avengers(logan, 0).
+avengers(ultron, 0).
+avengers(quicksilver, 0).
+avengers(deadpool, 0).
+avengers(bucky, 0).
+avengers(venom, 0).
+avengers(red_skull, 0).
+avengers(captain_marvel, 0).
+avengers(doctor_octopus, 0).
+avengers(falcon, 1).
+avengers(rocket_raccoon, 0).
+avengers(green_goblin, 0).
+avengers(ant-man, 1).
+
+avengers(juggernaut, 0).
+avengers(odin, 0).
+avengers(mephisto, 0).
+avengers(nebula, 0).
+avengers(shuri, 0).
+avengers(phil_coulson, 0).
+avengers(wong, 0).
+avengers(drax, 0).
+avengers(mantis, 0).
+avengers(valkyrie, 0).
+
+
+
+spider_enemy(spider-man, 0).
+spider_enemy(iron_man, 0).
+spider_enemy(hulk, 0).
+spider_enemy(сaptain_america, 0).
+spider_enemy(black_widow, 0).
+spider_enemy(thor, 0).
+spider_enemy(scarlet_witch, 0).
+spider_enemy(doctor_strange, 0).
+spider_enemy(clint, 0).
+spider_enemy(black_panther, 0).
+spider_enemy(war_machine, 0).
+spider_enemy(vision, 0).
+spider_enemy(nick_fury, 0).
+spider_enemy(thanos, 0).
+spider_enemy(groot, 0).
+spider_enemy(gamora, 0).
+spider_enemy(loki, 0).
+spider_enemy(logan, 0).
+spider_enemy(ultron, 0).
+spider_enemy(quicksilver, 0).
+spider_enemy(deadpool, 0).
+spider_enemy(bucky, 0).
+spider_enemy(venom, 1).
+spider_enemy(red_skull, 0).
+spider_enemy(captain_marvel, 0).
+spider_enemy(doctor_octopus, 1).
+spider_enemy(falcon, 0).
+spider_enemy(rocket_raccoon, 0).
+spider_enemy(green_goblin, 1).
+spider_enemy(ant-man, 0).
+
+spider_enemy(juggernaut, 0).
+spider_enemy(odin, 0).
+spider_enemy(mephisto, 0).
+spider_enemy(nebula, 0).
+spider_enemy(shuri, 0).
+spider_enemy(phil_coulson, 0).
+spider_enemy(wong, 0).
+spider_enemy(drax, 0).
+spider_enemy(mantis, 0).
+spider_enemy(valkyrie, 0).
+
+
+
+mask(spider-man, 1).
+mask(iron_man, 1).
+mask(hulk, 0).
+mask(сaptain_america, 1).
+mask(black_widow, 0).
+mask(thor, 0).
+mask(scarlet_witch, 0).
+mask(doctor_strange, 0).
+mask(clint, 0).
+mask(black_panther, 1).
+mask(war_machine, 1).
+mask(vision, 0).
+mask(nick_fury, 2).
+mask(thanos, 0).
+mask(groot, 0).
+mask(gamora, 0).
+mask(loki, 0).
+mask(logan, 0).
+mask(ultron, 0).
+mask(quicksilver, 0).
+mask(deadpool, 1).
+mask(bucky, 1).
+mask(venom, 1).
+mask(red_skull, 1).
+mask(captain_marvel, 0).
+mask(doctor_octopus, 0).
+mask(falcon, 0).
+mask(rocket_raccoon, 0).
+mask(green_goblin, 1).
+mask(ant-man, 1).
+
+mask(juggernaut, 1).
+mask(odin, 2).
+mask(mephisto, 0).
+mask(nebula, 0).
+mask(shuri, 0).
+mask(phil_coulson, 0).
+mask(wong, 0).
+mask(drax, 0).
+mask(mantis, 0).
+mask(valkyrie, 0).
+
+superpowers(spider-man, 1).
+superpowers(iron_man, 0).
+superpowers(hulk, 1).
+superpowers(сaptain_america, 1).
+superpowers(black_widow, 0).
+superpowers(thor, 1).
+superpowers(scarlet_witch, 1).
+superpowers(doctor_strange, 1).
+superpowers(clint, 0).
+superpowers(black_panther, 1).
+superpowers(war_machine, 0).
+superpowers(vision, 1).
+superpowers(nick_fury, 0).
+superpowers(thanos, 0).
+superpowers(groot, 0).
+superpowers(gamora, 0).
+superpowers(loki, 1).
+superpowers(logan, 1).
+superpowers(ultron, 0).
+superpowers(quicksilver, 1).
+superpowers(deadpool, 1).
+superpowers(bucky, 0).
+superpowers(venom, 1).
+superpowers(red_skull, 0).
+superpowers(captain_marvel, 1).
+superpowers(doctor_octopus, 0).
+superpowers(falcon, 0).
+superpowers(rocket_raccoon, 0).
+superpowers(green_goblin, 0).
+superpowers(ant-man, 0).
+
+superpowers(juggernaut, 1).
+superpowers(odin, 1).
+superpowers(mephisto, 1).
+superpowers(nebula, 0).
+superpowers(shuri, 0).
+superpowers(phil_coulson, 0).
+superpowers(wong, 1).
+superpowers(drax, 0).
+superpowers(mantis, 0).
+superpowers(valkyrie, 0).
+
+
+scientist(spider-man, 0).
+scientist(iron_man, 1).
+scientist(hulk, 1).
+scientist(сaptain_america, 0).
+scientist(black_widow, 0).
+scientist(thor, 0).
+scientist(scarlet_witch, 0).
+scientist(doctor_strange, 0).
+scientist(clint, 0).
+scientist(black_panther, 0).
+scientist(war_machine, 0).
+scientist(vision, 0).
+scientist(nick_fury, 0).
+scientist(thanos, 0).
+scientist(groot, 0).
+scientist(gamora, 0).
+scientist(loki, 0).
+scientist(logan, 0).
+scientist(ultron, 0).
+scientist(quicksilver, 0).
+scientist(deadpool, 0).
+scientist(bucky, 0).
+scientist(venom, 0).
+scientist(red_skull, 0).
+scientist(captain_marvel, 0).
+scientist(doctor_octopus, 1).
+scientist(falcon, 0).
+scientist(rocket_raccoon, 0).
+scientist(green_goblin, 1).
+scientist(ant-man, 0).
+
+scientist(juggernaut, 0).
+scientist(odin, 0).
+scientist(mephisto, 0).
+scientist(nebula, 0).
+scientist(shuri, 1).
+scientist(phil_coulson, 0).
+scientist(wong, 0).
+scientist(drax, 0).
+scientist(mantis, 0).
+scientist(valkyrie, 0).
+
+regeneration(spider-man, 0).
+regeneration(iron_man, 0).
+regeneration(hulk, 1).
+regeneration(сaptain_america, 0).
+regeneration(black_widow, 0).
+regeneration(thor, 0).
+regeneration(scarlet_witch, 0).
+regeneration(doctor_strange, 0).
+regeneration(clint, 0).
+regeneration(black_panther, 0).
+regeneration(war_machine, 0).
+regeneration(vision, 0).
+regeneration(nick_fury, 0).
+regeneration(thanos, 0).
+regeneration(groot, 0).
+regeneration(gamora, 0).
+regeneration(loki, 0).
+regeneration(logan, 1).
+regeneration(ultron, 0).
+regeneration(quicksilver, 0).
+regeneration(deadpool, 1).
+regeneration(bucky, 0).
+regeneration(venom, 1).
+regeneration(red_skull, 0).
+regeneration(captain_marvel, 1).
+regeneration(doctor_octopus, 0).
+regeneration(falcon, 0).
+regeneration(rocket_raccoon, 0).
+regeneration(green_goblin, 0).
+regeneration(ant-man, 0).
+
+regeneration(juggernaut, 1).
+regeneration(odin, 0).
+regeneration(mephisto, 0).
+regeneration(nebula, 0).
+regeneration(shuri, 0).
+regeneration(phil_coulson, 0).
+regeneration(wong, 0).
+regeneration(drax, 0).
+regeneration(mantis, 0).
+regeneration(valkyrie, 0).
+
+
+magic(spider-man, 0).
+magic(iron_man, 0).
+magic(hulk, 0).
+magic(сaptain_america, 0).
+magic(black_widow, 0).
+magic(thor, 0).
+magic(scarlet_witch, 1).
+magic(doctor_strange, 1).
+magic(clint, 0).
+magic(black_panther, 0).
+magic(war_machine, 0).
+magic(vision, 0).
+magic(nick_fury, 0).
+magic(thanos, 0).
+magic(groot, 0).
+magic(gamora, 0).
+magic(loki, 1).
+magic(logan, 0).
+magic(ultron, 0).
+magic(quicksilver, 0).
+magic(deadpool, 0).
+magic(bucky, 0).
+magic(venom, 0).
+magic(red_skull, 0).
+magic(captain_marvel, 1).
+magic(doctor_octopus, 0).
+magic(falcon, 0).
+magic(rocket_raccoon, 0).
+magic(green_goblin, 0).
+magic(ant-man, 0).
+
+magic(juggernaut, 0).
+magic(odin, 0).
+magic(mephisto, 1).
+magic(nebula, 0).
+magic(shuri, 0).
+magic(phil_coulson, 0).
+magic(wong, 1).
+magic(drax, 0).
+magic(mantis, 1).
+magic(valkyrie, 0).
+
+
+agent(spider-man, 0).
+agent(iron_man, 0).
+agent(hulk, 0).
+agent(сaptain_america, 0).
+agent(black_widow, 1).
+agent(thor, 0).
+agent(scarlet_witch, 0).
+agent(doctor_strange, 0).
+agent(clint, 1).
+agent(black_panther, 0).
+agent(war_machine, 0).
+agent(vision, 0).
+agent(nick_fury, 1).
+agent(thanos, 0).
+agent(groot, 0).
+agent(gamora, 0).
+agent(loki, 0).
+agent(logan, 0).
+agent(ultron, 0).
+agent(quicksilver, 0).
+agent(deadpool, 0).
+agent(bucky, 1).
+agent(venom, 0).
+agent(red_skull, 0).
+agent(captain_marvel, 0).
+agent(doctor_octopus, 0).
+agent(falcon, 1).
+agent(rocket_raccoon, 0).
+agent(green_goblin, 0).
+agent(ant-man, 0).
+
+agent(juggernaut, 0).
+agent(odin, 0).
+agent(mephisto, 0).
+agent(nebula, 0).
+agent(shuri, 0).
+agent(phil_coulson, 1).
+agent(wong, 0).
+agent(drax, 0).
+agent(mantis, 0).
+agent(valkyrie, 0).
+
+
+woman(spider-man, 0).
+woman(iron_man, 0).
+woman(hulk, 0).
+woman(сaptain_america, 0).
+woman(black_widow, 1).
+woman(thor, 0).
+woman(scarlet_witch, 1).
+woman(doctor_strange, 0).
+woman(clint, 0).
+woman(black_panther, 0).
+woman(war_machine, 0).
+woman(vision, 2).
+woman(nick_fury, 0).
+woman(thanos, 0).
+woman(groot, 2).
+woman(gamora, 1).
+woman(loki, 0).
+woman(logan, 0).
+woman(ultron, 2).
+woman(quicksilver, 0).
+woman(deadpool, 0).
+woman(bucky, 0).
+woman(venom, 0).
+woman(red_skull, 0).
+woman(captain_marvel, 1).
+woman(doctor_octopus, 0).
+woman(falcon, 0).
+woman(rocket_raccoon, 0).
+woman(green_goblin, 0).
+woman(ant-man, 0).
+
+woman(juggernaut, 0).
+woman(odin, 0).
+woman(mephisto, 0).
+woman(nebula, 1).
+woman(shuri, 1).
+woman(phil_coulson, 0).
+woman(wong, 0).
+woman(drax, 0).
+woman(mantis, 1).
+woman(valkyrie, 1).
+
+
+
+web(spider-man, 1).
+web(iron_man, 0).
+web(hulk, 0).
+web(сaptain_america, 0).
+web(black_widow, 0).
+web(thor, 0).
+web(scarlet_witch, 0).
+web(doctor_strange, 0).
+web(clint, 0).
+web(black_panther, 0).
+web(war_machine, 0).
+web(vision, 0).
+web(nick_fury, 0).
+web(thanos, 0).
+web(groot, 0).
+web(gamora, 0).
+web(loki, 0).
+web(logan, 0).
+web(ultron, 0).
+web(quicksilver, 0).
+web(deadpool, 0).
+web(bucky, 0).
+web(venom, 0).
+web(red_skull, 0).
+web(captain_marvel, 0).
+web(doctor_octopus, 0).
+web(falcon, 0).
+web(rocket_raccoon, 0).
+web(green_goblin, 0).
+web(ant-man, 0).
+
+web(juggernaut, 0).
+web(odin, 0).
+web(mephisto, 0).
+web(nebula, 0).
+web(shuri, 0).
+web(phil_coulson, 0).
+web(wong, 0).
+web(drax, 0).
+web(mantis, 0).
+web(valkyrie, 0).
+
+
+
+question1(X1):-	write("Ваш персонаж умеет летать?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X1).
+
+question2(X2):-	write("Ваш персонаж на стороне зла?"),nl,
+                                write("2. Нейтрален"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X2).
+
+question3(X3):-	write("Ваш персонаж является человеком?"),nl,
+                                write("2. Наполовину"),nl,
+                                write("1. Да"),nl,
+				write("0. Нeт"),nl,
+				read(X3).
+
+question4(X4):-	write("Ваш персонаж может изменять свои размеры?"),nl,
+				write("1.Да"),nl,
+				write("0. Нет"),nl,
+				read(X4).
+
+question5(X5):-	write("Ваш персонаж родился на Земле?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X5).
+
+question6(X6):-	write("Ваш персонаж входил в состав мстителей?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X6).
+
+question7(X7):-	write("Ваш персонаж был врагом человека паука?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X7).
+
+question8(X8):-	write("Ваш персонаж носил маску?"),nl,
+                                write("2. Носил маску"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X8).
+
+question9(X9):-	write("Ваш персонаж имеет суперспособности?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X9).
+
+question10(X10):-write("Ваш персонаж женщина?"),nl,
+                                write("2. Не живое"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X10).
+
+question11(X11):-write("Ваш персонаж ученый?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X11).
+
+question12(X12):-write("Ваш персонаж имеет регенерацию?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X12).
+
+question13(X13):-write("Ваш персонаж обладает магией?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X13).
+
+question14(X14):-write("Ваш персонаж агент?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X14).
+
+question15(X15):-write("Ваш персонаж разбрасывает паутину?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X15).
+
+
+
+pr:-	question10(X10), question1(X1),question2(X2),question3(X3),question4(X4),
+		question5(X5),question6(X6),question7(X7),question8(X8),
+                question9(X9),question11(X11),question12(X12),question13(X13),
+                question14(X14),question15(X15),
+
+		fly(X,X1),evil(X,X2),man(X,X3),size(X,X4),
+		earth(X,X5),avengers(X,X6),spider_enemy(X,X7),mask(X,X8),
+                superpowers(X,X9),woman(X,X10),scientist(X,X11),
+                regeneration(X,X12),magic(X,X13),agent(X,X14),web(X,X15),
+		write(X),nl,fail.
