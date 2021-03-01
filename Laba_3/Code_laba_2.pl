190c1465a17998db4a91d89530d85bb5fcb27414
KonstantinFisun <72182894+KonstantinFisun@users.noreply.github.com>
2021-03-01 23:06:10 +0300

    ЛБ 3 Задание 15

---
+++ Laba_3/Code_laba_2.pl
@@ -0,0 +1,791 @@
+%Высказывание quest(+Question,+Hero,+Answer)
+%Question - номер соответствующего вопроса
+%Hero - имя персонажа
+%Answer - Ответ
+%fly
+
+quest(1,spider-man, 0).
+quest(1,iron_man, 1).
+quest(1,hulk, 0).
+quest(1,сaptain_america, 0).
+quest(1,black_widow, 0).
+quest(1,thor, 1).
+quest(1,scarlet_witch, 1).
+quest(1,doctor_strange, 1).
+quest(1,clint, 0).
+quest(1,black_panther, 0).
+quest(1,war_machine, 1).
+quest(1,vision, 1).
+quest(1,nick_fury, 0).
+quest(1,thanos, 0).
+quest(1,groot, 0).
+quest(1,gamora, 0).
+quest(1,loki, 0).
+quest(1,logan, 0).
+quest(1,ultron, 1).
+quest(1,quicksilver, 0).
+quest(1,deadpool, 0).
+quest(1,bucky, 0).
+quest(1,venom, 0).
+quest(1,red_skull, 0).
+quest(1,captain_marvel, 1).
+quest(1,doctor_octopus, 0).
+quest(1,falcon, 1).
+quest(1,rocket_raccoon, 0).
+quest(1,green_goblin, 0).
+quest(1,ant - quest, 0).
+
+quest(1,juggernaut, 0).
+quest(1,odin, 0).
+quest(1,mephisto, 0).
+quest(1,nebula, 0).
+quest(1,shuri, 0).
+quest(1,phil_coulson, 0).
+quest(1,wong, 0).
+quest(1,drax, 0).
+quest(1,mantis, 0).
+quest(1,valkyrie, 0).
+
+
+%evil.
+
+quest(2,spider-man, 0).
+quest(2,iron_man, 0).
+quest(2,hulk, 2).
+quest(2,сaptain_america, 0).
+quest(2,black_widow, 0).
+quest(2,thor, 0).
+quest(2,scarlet_witch, 0).
+quest(2,doctor_strange, 0).
+quest(2,clint, 0).
+quest(2,black_panther, 0).
+quest(2,war_machine, 0).
+quest(2,vision, 0).
+quest(2,nick_fury, 0).
+quest(2,thanos, 1).
+quest(2,groot, 0).
+quest(2,gamora, 0).
+quest(2,loki, 1).
+quest(2,logan, 0).
+quest(2,ultron, 1).
+quest(2,quicksilver, 0).
+quest(2,deadpool, 0).
+quest(2,bucky, 0).
+quest(2,venom, 1).
+quest(2,red_skull, 1).
+quest(2,captain_marvel, 0).
+quest(2,doctor_octopus, 1).
+quest(2,falcon, 0).
+quest(2,rocket_raccoon, 0).
+quest(2,green_goblin, 1).
+quest(2,ant - quest, 0).
+
+quest(2,juggernaut, 1).
+quest(2,odin, 0).
+quest(2,mephisto, 1).
+quest(2,nebula, 2).
+quest(2,shuri, 0).
+quest(2,phil_coulson, 0).
+quest(2,wong, 0).
+quest(2,drax, 0).
+quest(2,mantis, 0).
+quest(2,valkyrie, 0).
+
+
+%man.
+
+quest(3,spider-man, 1).
+quest(3,iron_man, 1).
+quest(3,hulk, 2).
+quest(3,сaptain_america, 1).
+quest(3,black_widow, 1).
+quest(3,thor, 2).
+quest(3,scarlet_witch, 1).
+quest(3,doctor_strange, 1).
+quest(3,clint, 1).
+quest(3,black_panther, 1).
+quest(3,war_machine, 1).
+quest(3,vision, 0).
+quest(3,nick_fury, 1).
+quest(3,thanos, 0).
+quest(3,groot, 0).
+quest(3,gamora, 0).
+quest(3,loki, 2).
+quest(3,logan, 1).
+quest(3,ultron, 0).
+quest(3,quicksilver, 1).
+quest(3,deadpool, 1).
+quest(3,bucky, 1).
+quest(3,venom, 0).
+quest(3,red_skull, 1).
+quest(3,captain_marvel, 1).
+quest(3,doctor_octopus, 1).
+quest(3,falcon, 1).
+quest(3,rocket_raccoon, 0).
+quest(3,green_goblin, 1).
+quest(3,ant - quest, 1).
+
+quest(3,juggernaut, 1).
+quest(3,odin, 0).
+quest(3,mephisto, 0).
+quest(3,nebula, 0).
+quest(3,shuri, 1).
+quest(3,phil_coulson, 1).
+quest(3,wong, 1).
+quest(3,drax, 0).
+quest(3,mantis, 0).
+quest(3,valkyrie, 2).
+
+
+%size.
+
+quest(4,spider - man, 0).
+quest(4,iron_man, 0).
+quest(4,hulk, 1).
+quest(4,сaptain_america, 0).
+quest(4,black_widow, 0).
+quest(4,thor, 0).
+quest(4,scarlet_witch, 0).
+quest(4,doctor_strange, 0).
+quest(4,clint, 0).
+quest(4,black_panther, 0).
+quest(4,war_machine, 0).
+quest(4,vision, 0).
+quest(4,nick_fury, 0).
+quest(4,thanos, 0).
+quest(4,groot, 0).
+quest(4,gamora, 0).
+quest(4,loki, 0).
+quest(4,logan, 0).
+quest(4,ultron, 0).
+quest(4,quicksilver, 0).
+quest(4,deadpool, 0).
+quest(4,bucky, 0).
+quest(4,venom, 0).
+quest(4,red_skull, 0).
+quest(4,captain_marvel, 0).
+quest(4,doctor_octopus, 0).
+quest(4,falcon, 0).
+quest(4,rocket_raccoon, 0).
+quest(4,green_goblin, 0).
+quest(4,ant - quest, 1).
+
+quest(4,juggernaut, 0).
+quest(4,odin, 0).
+quest(4,mephisto, 0).
+quest(4,nebula, 0).
+quest(4,shuri, 0).
+quest(4,phil_coulson, 0).
+quest(4,wong, 0).
+quest(4,drax, 0).
+quest(4,mantis, 0).
+quest(4,valkyrie, 0).
+
+
+%earth.
+
+quest(5,spider - man, 1).
+quest(5,iron_man, 1).
+quest(5,hulk, 1).
+quest(5,сaptain_america, 1).
+quest(5,black_widow, 1).
+quest(5,thor, 0).
+quest(5,scarlet_witch, 1).
+quest(5,doctor_strange, 1).
+quest(5,clint, 1).
+quest(5,black_panther, 1).
+quest(5,war_machine, 1).
+quest(5,vision, 1).
+quest(5,nick_fury, 1).
+quest(5,thanos, 0).
+quest(5,groot, 0).
+quest(5,gamora, 0).
+quest(5,loki, 0).
+quest(5,logan, 1).
+quest(5,ultron, 1).
+quest(5,quicksilver, 1).
+quest(5,deadpool, 1).
+quest(5,bucky, 1).
+quest(5,venom, 0).
+quest(5,red_skull, 1).
+quest(5,captain_marvel, 1).
+quest(5,doctor_octopus, 1).
+quest(5,falcon, 1).
+quest(5,rocket_raccoon, 0).
+quest(5,green_goblin, 1).
+quest(5,ant - quest, 1).
+
+quest(5,juggernaut, 1).
+quest(5,odin, 0).
+quest(5,mephisto, 0).
+quest(5,nebula, 0).
+quest(5,shuri, 1).
+quest(5,phil_coulson, 1).
+quest(5,wong, 1).
+quest(5,drax, 1).
+quest(5,mantis, 0).
+quest(5,valkyrie, 0).
+
+
+
+%avengers.
+
+quest(6,spider - man, 0).
+quest(6,iron_man, 1).
+quest(6,hulk, 1).
+quest(6,сaptain_america, 1).
+quest(6,black_widow, 1).
+quest(6,thor, 1).
+quest(6,scarlet_witch, 0).
+quest(6,doctor_strange, 0).
+quest(6,clint, 1).
+quest(6,black_panther, 0).
+quest(6,war_machine, 0).
+quest(6,vision, 0).
+quest(6,nick_fury, 0).
+quest(6,thanos, 0).
+quest(6,groot, 0).
+quest(6,gamora, 0).
+quest(6,loki, 0).
+quest(6,logan, 0).
+quest(6,ultron, 0).
+quest(6,quicksilver, 0).
+quest(6,deadpool, 0).
+quest(6,bucky, 0).
+quest(6,venom, 0).
+quest(6,red_skull, 0).
+quest(6,captain_marvel, 0).
+quest(6,doctor_octopus, 0).
+quest(6,falcon, 1).
+quest(6,rocket_raccoon, 0).
+quest(6,green_goblin, 0).
+quest(6,ant - quest, 1).
+
+quest(6,juggernaut, 0).
+quest(6,odin, 0).
+quest(6,mephisto, 0).
+quest(6,nebula, 0).
+quest(6,shuri, 0).
+quest(6,phil_coulson, 0).
+quest(6,wong, 0).
+quest(6,drax, 0).
+quest(6,mantis, 0).
+quest(6,valkyrie, 0).
+
+
+%spider_enemy.
+
+quest(7,spider - man, 0).
+quest(7,iron_man, 0).
+quest(7,hulk, 0).
+quest(7,сaptain_america, 0).
+quest(7,black_widow, 0).
+quest(7,thor, 0).
+quest(7,scarlet_witch, 0).
+quest(7,doctor_strange, 0).
+quest(7,clint, 0).
+quest(7,black_panther, 0).
+quest(7,war_machine, 0).
+quest(7,vision, 0).
+quest(7,nick_fury, 0).
+quest(7,thanos, 0).
+quest(7,groot, 0).
+quest(7,gamora, 0).
+quest(7,loki, 0).
+quest(7,logan, 0).
+quest(7,ultron, 0).
+quest(7,quicksilver, 0).
+quest(7,deadpool, 0).
+quest(7,bucky, 0).
+quest(7,venom, 1).
+quest(7,red_skull, 0).
+quest(7,captain_marvel, 0).
+quest(7,doctor_octopus, 1).
+quest(7,falcon, 0).
+quest(7,rocket_raccoon, 0).
+quest(7,green_goblin, 1).
+quest(7,ant - quest, 0).
+
+quest(7,juggernaut, 0).
+quest(7,odin, 0).
+quest(7,mephisto, 0).
+quest(7,nebula, 0).
+quest(7,shuri, 0).
+quest(7,phil_coulson, 0).
+quest(7,wong, 0).
+quest(7,drax, 0).
+quest(7,mantis, 0).
+quest(7,valkyrie, 0).
+
+
+%mask.
+
+quest(8,spider-man, 1).
+quest(8,iron_man, 1).
+quest(8,hulk, 0).
+quest(8,сaptain_america, 1).
+quest(8,black_widow, 0).
+quest(8,thor, 0).
+quest(8,scarlet_witch, 0).
+quest(8,doctor_strange, 0).
+quest(8,clint, 0).
+quest(8,black_panther, 1).
+quest(8,war_machine, 1).
+quest(8,vision, 0).
+quest(8,nick_fury, 2).
+quest(8,thanos, 0).
+quest(8,groot, 0).
+quest(8,gamora, 0).
+quest(8,loki, 0).
+quest(8,logan, 0).
+quest(8,ultron, 0).
+quest(8,quicksilver, 0).
+quest(8,deadpool, 1).
+quest(8,bucky, 1).
+quest(8,venom, 1).
+quest(8,red_skull, 1).
+quest(8,captain_marvel, 0).
+quest(8,doctor_octopus, 0).
+quest(8,falcon, 0).
+quest(8,rocket_raccoon, 0).
+quest(8,green_goblin, 1).
+quest(8,ant - quest, 1).
+
+quest(8,juggernaut, 1).
+quest(8,odin, 2).
+quest(8,mephisto, 0).
+quest(8,nebula, 0).
+quest(8,shuri, 0).
+quest(8,phil_coulson, 0).
+quest(8,wong, 0).
+quest(8,drax, 0).
+quest(8,mantis, 0).
+quest(8,valkyrie, 0).
+
+
+%superpowers.
+
+quest(9,spider-man, 1).
+quest(9,iron_man, 0).
+quest(9,hulk, 1).
+quest(9,сaptain_america, 1).
+quest(9,black_widow, 0).
+quest(9,thor, 1).
+quest(9,scarlet_witch, 1).
+quest(9,doctor_strange, 1).
+quest(9,clint, 0).
+quest(9,black_panther, 1).
+quest(9,war_machine, 0).
+quest(9,vision, 1).
+quest(9,nick_fury, 0).
+quest(9,thanos, 0).
+quest(9,groot, 0).
+quest(9,gamora, 0).
+quest(9,loki, 1).
+quest(9,logan, 1).
+quest(9,ultron, 0).
+quest(9,quicksilver, 1).
+quest(9,deadpool, 1).
+quest(9,bucky, 0).
+quest(9,venom, 1).
+quest(9,red_skull, 0).
+quest(9,captain_marvel, 1).
+quest(9,doctor_octopus, 0).
+quest(9,falcon, 0).
+quest(9,rocket_raccoon, 0).
+quest(9,green_goblin, 0).
+quest(9,ant - quest, 0).
+
+quest(9,juggernaut, 1).
+quest(9,odin, 1).
+quest(9,mephisto, 1).
+quest(9,nebula, 0).
+quest(9,shuri, 0).
+quest(9,phil_coulson, 0).
+quest(9,wong, 1).
+quest(9,drax, 0).
+quest(9,mantis, 0).
+quest(9,valkyrie, 0).
+
+
+%scientist.
+
+quest(10,spider-man, 0).
+quest(10,iron_man, 1).
+quest(10,hulk, 1).
+quest(10,сaptain_america, 0).
+quest(10,black_widow, 0).
+quest(10,thor, 0).
+quest(10,scarlet_witch, 0).
+quest(10,doctor_strange, 0).
+quest(10,clint, 0).
+quest(10,black_panther, 0).
+quest(10,war_machine, 0).
+quest(10,vision, 0).
+quest(10,nick_fury, 0).
+quest(10,thanos, 0).
+quest(10,groot, 0).
+quest(10,gamora, 0).
+quest(10,loki, 0).
+quest(10,logan, 0).
+quest(10,ultron, 0).
+quest(10,quicksilver, 0).
+quest(10,deadpool, 0).
+quest(10,bucky, 0).
+quest(10,venom, 0).
+quest(10,red_skull, 0).
+quest(10,captain_marvel, 0).
+quest(10,doctor_octopus, 1).
+quest(10,falcon, 0).
+quest(10,rocket_raccoon, 0).
+quest(10,green_goblin, 1).
+quest(10,ant - quest, 0).
+
+quest(10,juggernaut, 0).
+quest(10,odin, 0).
+quest(10,mephisto, 0).
+quest(10,nebula, 0).
+quest(10,shuri, 1).
+quest(10,phil_coulson, 0).
+quest(10,wong, 0).
+quest(10,drax, 0).
+quest(10,mantis, 0).
+quest(10,valkyrie, 0).
+
+
+%regeneration.
+
+quest(11,spider-man, 0).
+quest(11,iron_man, 0).
+quest(11,hulk, 1).
+quest(11,сaptain_america, 0).
+quest(11,black_widow, 0).
+quest(11,thor, 0).
+quest(11,scarlet_witch, 0).
+quest(11,doctor_strange, 0).
+quest(11,clint, 0).
+quest(11,black_panther, 0).
+quest(11,war_machine, 0).
+quest(11,vision, 0).
+quest(11,nick_fury, 0).
+quest(11,thanos, 0).
+quest(11,groot, 0).
+quest(11,gamora, 0).
+quest(11,loki, 0).
+quest(11,logan, 1).
+quest(11,ultron, 0).
+quest(11,quicksilver, 0).
+quest(11,deadpool, 1).
+quest(11,bucky, 0).
+quest(11,venom, 1).
+quest(11,red_skull, 0).
+quest(11,captain_marvel, 1).
+quest(11,doctor_octopus, 0).
+quest(11,falcon, 0).
+quest(11,rocket_raccoon, 0).
+quest(11,green_goblin, 0).
+quest(11,ant - quest, 0).
+
+quest(11,juggernaut, 1).
+quest(11,odin, 0).
+quest(11,mephisto, 0).
+quest(11,nebula, 0).
+quest(11,shuri, 0).
+quest(11,phil_coulson, 0).
+quest(11,wong, 0).
+quest(11,drax, 0).
+quest(11,mantis, 0).
+quest(11,valkyrie, 0).
+
+
+%magic.
+
+quest(12,spider-man, 0).
+quest(12,iron_man, 0).
+quest(12,hulk, 0).
+quest(12,сaptain_america, 0).
+quest(12,black_widow, 0).
+quest(12,thor, 0).
+quest(12,scarlet_witch, 1).
+quest(12,doctor_strange, 1).
+quest(12,clint, 0).
+quest(12,black_panther, 0).
+quest(12,war_machine, 0).
+quest(12,vision, 0).
+quest(12,nick_fury, 0).
+quest(12,thanos, 0).
+quest(12,groot, 0).
+quest(12,gamora, 0).
+quest(12,loki, 1).
+quest(12,logan, 0).
+quest(12,ultron, 0).
+quest(12,quicksilver, 0).
+quest(12,deadpool, 0).
+quest(12,bucky, 0).
+quest(12,venom, 0).
+quest(12,red_skull, 0).
+quest(12,captain_marvel, 1).
+quest(12,doctor_octopus, 0).
+quest(12,falcon, 0).
+quest(12,rocket_raccoon, 0).
+quest(12,green_goblin, 0).
+quest(12,ant - quest, 0).
+
+quest(12,juggernaut, 0).
+quest(12,odin, 0).
+quest(12,mephisto, 1).
+quest(12,nebula, 0).
+quest(12,shuri, 0).
+quest(12,phil_coulson, 0).
+quest(12,wong, 1).
+quest(12,drax, 0).
+quest(12,mantis, 1).
+quest(12,valkyrie, 0).
+
+
+%agent.
+
+quest(13,spider-man, 0).
+quest(13,iron_man, 0).
+quest(13,hulk, 0).
+quest(13,сaptain_america, 0).
+quest(13,black_widow, 1).
+quest(13,thor, 0).
+quest(13,scarlet_witch, 0).
+quest(13,doctor_strange, 0).
+quest(13,clint, 1).
+quest(13,black_panther, 0).
+quest(13,war_machine, 0).
+quest(13,vision, 0).
+quest(13,nick_fury, 1).
+quest(13,thanos, 0).
+quest(13,groot, 0).
+quest(13,gamora, 0).
+quest(13,loki, 0).
+quest(13,logan, 0).
+quest(13,ultron, 0).
+quest(13,quicksilver, 0).
+quest(13,deadpool, 0).
+quest(13,bucky, 1).
+quest(13,venom, 0).
+quest(13,red_skull, 0).
+quest(13,captain_marvel, 0).
+quest(13,doctor_octopus, 0).
+quest(13,falcon, 1).
+quest(13,rocket_raccoon, 0).
+quest(13,green_goblin, 0).
+quest(13,ant - quest, 0).
+
+quest(13,juggernaut, 0).
+quest(13,odin, 0).
+quest(13,mephisto, 0).
+quest(13,nebula, 0).
+quest(13,shuri, 0).
+quest(13,phil_coulson, 1).
+quest(13,wong, 0).
+quest(13,drax, 0).
+quest(13,mantis, 0).
+quest(13,valkyrie, 0).
+
+
+%woman.
+
+quest(14,spider-man, 0).
+quest(14,iron_man, 0).
+quest(14,hulk, 0).
+quest(14,сaptain_america, 0).
+quest(14,black_widow, 1).
+quest(14,thor, 0).
+quest(14,scarlet_witch, 1).
+quest(14,doctor_strange, 0).
+quest(14,clint, 0).
+quest(14,black_panther, 0).
+quest(14,war_machine, 0).
+quest(14,vision, 2).
+quest(14,nick_fury, 0).
+quest(14,thanos, 0).
+quest(14,groot, 2).
+quest(14,gamora, 1).
+quest(14,loki, 0).
+quest(14,logan, 0).
+quest(14,ultron, 2).
+quest(14,quicksilver, 0).
+quest(14,deadpool, 0).
+quest(14,bucky, 0).
+quest(14,venom, 0).
+quest(14,red_skull, 0).
+quest(14,captain_marvel, 1).
+quest(14,doctor_octopus, 0).
+quest(14,falcon, 0).
+quest(14,rocket_raccoon, 0).
+quest(14,green_goblin, 0).
+quest(14,ant - quest, 0).
+
+quest(14,juggernaut, 0).
+quest(14,odin, 0).
+quest(14,mephisto, 0).
+quest(14,nebula, 1).
+quest(14,shuri, 1).
+quest(14,phil_coulson, 0).
+quest(14,wong, 0).
+quest(14,drax, 0).
+quest(14,mantis, 1).
+quest(14,valkyrie, 1).
+
+
+%web.
+
+quest(15,spider-man, 1).
+quest(15,iron_man, 0).
+quest(15,hulk, 0).
+quest(15,сaptain_america, 0).
+quest(15,black_widow, 0).
+quest(15,thor, 0).
+quest(15,scarlet_witch, 0).
+quest(15,doctor_strange, 0).
+quest(15,clint, 0).
+quest(15,black_panther, 0).
+quest(15,war_machine, 0).
+quest(15,vision, 0).
+quest(15,nick_fury, 0).
+quest(15,thanos, 0).
+quest(15,groot, 0).
+quest(15,gamora, 0).
+quest(15,loki, 0).
+quest(15,logan, 0).
+quest(15,ultron, 0).
+quest(15,quicksilver, 0).
+quest(15,deadpool, 0).
+quest(15,bucky, 0).
+quest(15,venom, 0).
+quest(15,red_skull, 0).
+quest(15,captain_marvel, 0).
+quest(15,doctor_octopus, 0).
+quest(15,falcon, 0).
+quest(15,rocket_raccoon, 0).
+quest(15,green_goblin, 0).
+quest(15,ant - quest, 0).
+
+quest(15,juggernaut, 0).
+quest(15,odin, 0).
+quest(15,mephisto, 0).
+quest(15,nebula, 0).
+quest(15,shuri, 0).
+quest(15,phil_coulson, 0).
+quest(15,wong, 0).
+quest(15,drax, 0).
+quest(15,mantis, 0).
+quest(15,valkyrie, 0).
+
+
+%Предикат quest(+Question,-Answer)
+%Question - номер вопроса, Answer - соответствующий ответ
+
+
+quest(1,X):-	write("Ваш персонаж умеет летать?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X).
+
+quest(2,X):-	write("Ваш персонаж на стороне зла?"),nl,
+                                write("2. Нейтрален"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X).
+
+quest(3,X):-	write("Ваш персонаж является человеком?"),nl,
+                                write("2. Наполовину"),nl,
+                                write("1. Да"),nl,
+				write("0. Нeт"),nl,
+				read(X).
+
+quest(4,X):-	write("Ваш персонаж может изменять свои размеры?"),nl,
+				write("1.Да"),nl,
+				write("0. Нет"),nl,
+				read(X).
+
+quest(5,X):-	write("Ваш персонаж родился на Земле?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X).
+
+quest(6,X):-	write("Ваш персонаж входил в состав мстителей?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X).
+
+quest(7,X):-	write("Ваш персонаж был врагом человека паука?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X).
+
+quest(8,X):-	write("Ваш персонаж носил маску?"),nl,
+                                write("2. Носил маску"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X).
+
+quest(9,X):-	write("Ваш персонаж имеет суперспособности?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X).
+
+
+
+quest(10,X):-write("Ваш персонаж ученый?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X).
+
+quest(11,X):-write("Ваш персонаж имеет регенерацию?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X).
+
+quest(12,X):-write("Ваш персонаж обладает магией?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X).
+
+quest(13,X):-write("Ваш персонаж агент?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X).
+
+quest(14,X):-write("Ваш персонаж женщина?"),nl,
+                                write("2. Не живое"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+				read(X).
+
+quest(15,X):-write("Ваш персонаж разбрасывает паутину?"),nl,
+				write("1. Да"),nl,
+				write("0. Нет"),nl,
+                                read(X).
+
+%	Предикат next_quest(+Question,+Answer,-New_question).
+%	Question - предыдущий номер вопроса и
+%       Answer - данный на него ответ
+%       New_question - номер следующего вопроса исходя из Question
+
+
+next_quest(1,1,8).
+next_quest(1,0,3).
+
+
+
+
+%Вызов акинатора
+pr:-pr(1,[spider-man,iron_man, hulk, сaptain_america, black_widow, thor, scarlet_witch, doctor_strange, clint, black_panther, war_machine,
+          vision, nick_fury, thanos, groot, gamora, loki, logan, ultron, quicksilver, deadpool, bucky, venom, red_skull, captain_marvel,
+          doctor_octopus, falcon, rocket_raccoon, green_goblin,ant-man]).
+
+
+pr(N,Marvel):-quest(N,X),check(N,X,Marvel,[],Marvel_other),(alone(Marvel_other)->write([Marvel_other]),!;N1 is N+1,pr(N1,Marvel_other)).
+
+%Находим совпадение по ответам на вопросы
+check(_,_,[],Other,Other):-!.
+check(N,X,[Hero|Tail],Other,Marvel_other):-(quest(N,Hero,X)->check(N,X,Tail,[Hero|Other],Marvel_other);check(N,X,Tail,Other,Marvel_other)).
+
+%Проверка на пустоту списка
+alone([_|Notalone]):-Notalone=[].
