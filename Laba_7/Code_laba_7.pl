1da6053bf7d4f71bec36a01abd339d772dd4afea
KonstantinFisun <72182894+KonstantinFisun@users.noreply.github.com>
2021-03-17 17:13:45 +0300

    ЛБ 7 Задание 3

--- Laba_7/Code_laba_7.pl
+++ Laba_7/Code_laba_7.pl
@@ -48,6 +48,68 @@ get_word([H|T],W,Word,New_Str_after_word):-append(W,[H],W1),get_word(T,W1,Word,N
 
 
 
+%Задание 3. Дана строка, определить самое частое слово
+
+% Предикат frequent_word(+Str,-Word) - возвращает самое частое слово
+% Word из строки Str
+frequent_word(Str,Word):-get_words(Str,Words),unique_elems(Words,Unique_words),
+		counts(Unique_words,C,Words),indOfMax(C,Ind),el_by_number(Unique_words,Ind,Word1),name(Word,Word1).
+
+
+
+%Предикат get_words(+Str,-Words) - получает список слов
+get_words(A,Words):-get_words(A,[],Words).
+
+get_words([],List_words,List_words):-!.
+get_words(Str,Words,List_words):-skip_space(Str,New_Str),get_word(New_Str,Word,New_Str_after_word),
+    Word \=[],append(Words,[Word],New_list_word),get_words(New_Str_after_word,New_list_word,List_words),!.
+get_words(_,List_words,List_words).
+
+
+
+
+%Предикат, который получает для данного элемента
+%количество раз, которое он встречается в списке.
+counts([],[],_):-!.
+counts([Word|T_words],[Count|T_counts],Words):-
+	count(Word,Words,Count),counts(T_words,T_counts,Words).
+
+count(_, [], 0):-!.
+count(Elem, List, X):- count(Elem, List, 0, X).
+count(_, [], Count, Count):- !.
+count(Elem, [Elem|T], Count, X):- Count1 is Count + 1, count(Elem, T, Count1, X), !.
+count(Elem, [_|T], Count, X):- count(Elem, T, Count, X).
+
+
+
+%Находим элемент под индексом
+el_by_number(A,Ind,El):-el_by_number(A,1,Ind,El).
+el_by_number([],_,_,nil):-!.
+el_by_number([El|_],Ind,Ind,El):-!.
+el_by_number([_|T],I,Ind,El):-I1 is I+1,el_by_number(T,I1,Ind,El).
+
+%Предикат построения списка из уникальных элементов
+unique_elems([], []):- !.
+unique_elems([H|T], List2):- unique_elems([H|T], List2, []).
+unique_elems([], CurList, CurList):- !.
+unique_elems([H|T], List, []):- unique_elems(T, List, [H]), !.
+unique_elems([H|T], List, CurList):-
+	not(contains(CurList, H)), append(CurList, [H], NewList), unique_elems(T, List, NewList), !.
+unique_elems([_|T], List, CurList):- unique_elems(T, List, CurList).
+
+contains([], _):- !, fail.
+contains([H|_], H):- !.
+contains([_|T], N):- contains(T, N).
+
+%Получаем индекс максимального элемента
+indOfMax(X,Y):-indexOfMin(X,Y).
+indexOfMin([], -1):- !.
+indexOfMin([H|T], X):-indexOfMin(T, 1, 1, X, H).
+indexOfMin([], _, MinInd, MinInd, _):-!.
+indexOfMin([H|T], CurInd, _, X, CurMin):-
+	H > CurMin, NewCurInd is CurInd + 1, indexOfMin(T, NewCurInd, NewCurInd, X, H), !.
+indexOfMin([_|T], CurInd, MinInd, X, CurMin):-
+	NewCurInd is CurInd + 1, indexOfMin(T, NewCurInd, MinInd, X, CurMin).
 
 
 
