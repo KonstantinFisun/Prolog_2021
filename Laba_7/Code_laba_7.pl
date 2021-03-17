55d9c47e56f055fba1710fbb000af4155e13f971
KonstantinFisun <72182894+KonstantinFisun@users.noreply.github.com>
2021-03-17 17:11:25 +0300

    ЛБ 7 Задание 2

--- Laba_7/Code_laba_7.pl
+++ Laba_7/Code_laba_7.pl
@@ -22,6 +22,30 @@ r_str(X,A,B,N,K):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
 pr_3_string:-read_str(Str,Lenght,0),write_str(Str),write(","),write_str(Str),write(","),write_str(Str),nl,write(Lenght).
 
 
+%Задание 2. Дана строка. Найти количество слов.
+
+%Предикат count_words(+Str,-Count) - подсчет количества слов в строке
+count_words(A,K):-count_words(A,0,K).
+
+%Предикат count_words(+Str,+Iter,-Count)
+%Iter - количество слов на определенном шаге
+count_words([],K,K):-!.%Остановка
+count_words(Str,I,K):-skip_space(Str,New_Str),get_word(New_Str,Word,New_Str_after_word),Word \=[],I1 is I+1,count_words(New_Str_after_word,I1,K),!.
+count_words(_,K,K).
+
+%Предикат skip_space(+Str,-New_Str) - пропуск пробелов в начале строки
+skip_space([32|Tail],New_Str):-skip_space(Tail,New_Str),!.
+skip_space(New_Str,New_Str).%Возврат строки без пробела
+
+% Предикат get_word(+Str,-Word,-New_Str_after_word) - считывает из
+% строки слово
+get_word([],[],[]):-!. %Остановка если пришла пустая строка
+get_word(Str,Word,New_Str_after_word):-get_word(Str,[],Word,New_Str_after_word).
+
+get_word([],Word,Word,[]).%Возвращаем пустую строку если после слова ничего нет
+get_word([32|T],Word,Word,T):-!. % Считываем до пробела и возвращаем строку без него
+get_word([H|T],W,Word,New_Str_after_word):-append(W,[H],W1),get_word(T,W1,Word,New_Str_after_word). % Считываем символы слова
+
 
 
 
